7d923bf94df6e9e782acaad42bd71ef35e750e3a
RaptorCentauri <dave.leo.shilander@icloud.com>
2017-10-03 01:02:39 -0500

    site files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,48 +9,94 @@
 $(document).ready(function() {
 
 
-
-function generateCharacters(){
-
-for(i=0; i<character.name.length; i++){
-	// myName = character.name[Math.floor(Math.random()*character.name.length)];
-	var characterButton = $("<button>");
-
-	characterButton.addClass("characterBtn");
-
-	characterButton.append("<h4><span></span></h4>");
-
-	$(".characterBtn h4 span").addClass("name");
-
-	characterButton.append("<img>");
-
-	$(".characterBtn  img").addClass("image");
-
-	characterButton.append("<h5><span></span></h5>");
-
-	$(".characterBtn h5 span").addClass("health");
-
-	$(".roster").append(characterButton);
-
-
-	$(".name").html(character.name[i]);
-	$(".health").html(character.health);
-	$(".image").attr("src", character.image[i]);
-
-	
-	}
-}
-
 var character = {
 	name: ["Batman", "Joker", "Red Hood", "Robin"],
 	image: ["assets/images/Batman.jpg", "assets/images/Joker.jpg", "assets/images/Redhood.jpg", "assets/images/Robin.jpg"],
-	health: Math.floor(Math.random()*(250-100 +1))+100,
+	health: Array.from({length: 4}, () => Math.floor(Math.random()*(250-100 +1))+100),
 	attack: Math.floor(Math.random()*(50-10 +1))+10,
 	counterAttack: Math.floor(Math.random()*(50-10 +1))+10,
 };
 
 
-generateCharacters();
+// Array.from({length: character.name.length}, () => Math.floor(Math.random()*(250-100 +1))+100);
+
+// $.each(obj, function (index, value) {
+//   console.log(value);
+// });
+
+function generateRoster(){
+
+	for(i=0; i<character.name.length; i++){
+
+			console.log("start: " + i);
+
+			//create a button
+			var characterButton = $("<button>");
+			characterButton.addClass("characterBtn");
+			characterButton.attr("data-name", character.name[i]);
+
+			//create the name label
+			characterButton.append("<h4><span></span></h4>");
+			$(".characterBtn h4 span").addClass("name");
+			console.log(character.name[i]);
+
+			//set the button image
+			characterButton.append("<img>");
+			$(".characterBtn  img").addClass("image");
+			console.log(character.image[i]);
+
+			//create the health label
+			characterButton.append("<h5><span></span></h5>");
+			$(".characterBtn h5 span").addClass("health");
+
+			//add the button the the roster
+
+			$(".roster").append(characterButton);
+			console.log("end: " + i);
+			console.log("==============================================");
+
+	}
+
+	$(".name").each(function(i){
+		$(this).html(character.name[i]);
+	})
+
+	$(".image").each(function(i){
+		$(this).attr("src", character.image[i]);
+	})
+
+	$(".health").each(function(i){
+		$(this).html(character.health[i]);
+	})
+
+
+	$(".characterBtn").on("click", function(){
+
+		if($("#currentCharacter").is(":empty")){
+		$("#currentCharacter").append(this);
+		}
+
+		else{
+			$("#defender").append(this);
+		}
+
+	});
+
+}
+
+
+
+
+
+
+// $(".characterBtn").on("click", function(){
+
+// 	$("#currentCharacter").append(characterButton);
+
+// });
+	
+generateRoster();
+
 
 
 
